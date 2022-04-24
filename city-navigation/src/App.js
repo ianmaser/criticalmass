f3158950eb02b6f04c55d6cc94c1ff5b43cb41e9
Ian Maser <ianmaser42@gmail.com>
2022-04-24 19:03:39 -0400

    Nav Items/links

--- city-navigation/src/App.js
+++ city-navigation/src/App.js
@@ -9,16 +9,40 @@ const PageWrapper = styled.div`
 
 const Navigation = styled.ul`
   display: flex;
+  list-style-type: none;
   width: 100%;
   background-color: red;
   flex-direction: row;
+  height: 50px;
+`;
+
+const NavItem = styled.li`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NavLink = styled.a`
+  display: block;
+  color: gray;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
 `;
 
 function App() {
   console.log('navigation', navigation);
   return (
     <PageWrapper>
-      <Navigation />
+      <Navigation> 
+        {navigation.cities.map((item, index) => {
+          return (
+          <NavItem>
+            <NavLink href={`#${item?.section}`}>{item.label}</NavLink> 
+          </NavItem>
+        );
+        })}
+      </Navigation>
     </PageWrapper>
   );
 }
