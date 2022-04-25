a9d3753e8be05e15de4e0f692596523d55dd37b7
Ian Maser <ianmaser42@gmail.com>
2022-04-24 23:07:39 -0400

    Active State/Time/Cleanup

--- city-navigation/src/App.js
+++ city-navigation/src/App.js
@@ -1,30 +1,29 @@
 import React, { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
-import navigation from './assets/navigation.json';
+import data from './data.json';
 
 const Navbar = styled.ul`
+  align-items: center;
   display: flex;
+  justify-content: center;
   list-style-type: none;
   width: 100%;
-  justify-content: center;
-  align-items: center;
 `;
 
-
 const NavItem = styled.li`
+  align-items: center;
   flex: 1;
   justify-content: center;
-  align-items: center;
 `;
 
 const NavLink = styled.a`
+  color: ${({active}) => active ? 'black' : 'gray'};
   display: inline-block;
   position: relative;
-  color: gray;
   text-align: center;
   text-decoration: none;
   &:hover {
-    color: blue;
+    color: ${({active}) => active ? 'black' : 'blue'};
   }
 `;
 
@@ -36,34 +35,61 @@ const NavLine = styled.hr`
   left: ${({ left }) => `${left}px`};
   width: ${({ lineWidth }) => `${lineWidth}px`};
   transition: left 400ms, width 300ms;
-`
+`;
 
 const StyledLine = styled.hr`
   background-color: lightgray;
   height: 2px;
   margin: 0;
   width: 100%;
-`
+`;
+
+const TimeWrapper = styled.div`
+  align-items: center;
+  border: 5px black solid;
+  display: flex;
+  justify-content: center;
+  padding: 10px 20px; 
+`;
+
+const TimeDisplay = styled.h1`
+  font-size: 50px;
+`;
 
 const App = () => {
   const [left, setLeft] = useState(0);
   const [lineWidth, setLineWidth] = useState();
+  const [active, setActive] = useState(0);
+  const [date, setDate] = useState(new Date())
   const refs = useRef([]);
+
+  const tick = () => setDate(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => tick(), 1000);
+
+    return function cleanup() { 
+      clearInterval(timerId);
+    };
+  });
 
   const handleClick = (index) => {
     const currentEl = refs.current[index].getBoundingClientRect();
-    setLeft(currentEl.left - 8);
+    setLeft(currentEl.left - 12);
     setLineWidth(currentEl.width + 8);
-    console.log(refs.current[index].getBoundingClientRect());
+    setActive(index);
   }
+
+  const currentTime = date.toLocaleString("en-US", { timeZone: data.cities[active].timeZone})
 
   return (
     <div className="App" style={{ position: 'relative'}}>
       <Navbar>
-        {navigation.cities.map((item, index) => {
+        {data.cities.map((item, index) => {
           return (
             <NavItem key={`Nav item-${index}`}>
               <NavLink
+                active={active === index}
                 href={`#${item?.section}`}
                 onClick={() => handleClick(index)} 
                 ref={(el) => refs.current[index] = el}
@@ -76,6 +102,11 @@ const App = () => {
       </Navbar>
       <NavLine left={left} lineWidth={lineWidth} />
       <StyledLine />
+      <TimeWrapper>
+        <TimeDisplay>
+          {currentTime}
+        </TimeDisplay>
+      </TimeWrapper>
     </div>
   );
 }
